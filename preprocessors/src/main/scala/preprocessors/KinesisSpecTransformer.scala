fb4cdfe5bce38549d378ab23c7f2ea4168c7ecbf
Eric Meisel <eric.steven.meisel@gmail.com>
2023-02-10 16:55:11 -0600

    Cleanup

--- preprocessors/src/main/scala/preprocessors/KinesisSpecTransformer.scala
+++ preprocessors/src/main/scala/preprocessors/KinesisSpecTransformer.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023-2023 etspaceman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package preprocessors
 
 import scala.jdk.CollectionConverters._
@@ -66,10 +82,10 @@ final class KinesisSpecTransformer extends ProjectionTransformer {
     } else shape
 
   def transform(context: TransformContext): Model = {
+    val transformer = context.getTransformer()
+
     val withMappedTraits =
-      context
-        .getTransformer()
-        .mapTraits(context.getModel(), traitTransform)
+      transformer.mapTraits(context.getModel(), traitTransform)
 
     val newShapes = withMappedTraits
       .shapes()
@@ -77,10 +93,10 @@ final class KinesisSpecTransformer extends ProjectionTransformer {
       .asScala
       .toList :+ nonNegativeIntegerObjectShape
 
-    val withNewShapes =
-      context.getTransformer().replaceShapes(withMappedTraits, newShapes.asJava)
-
-    context.getTransformer().mapShapes(withNewShapes, shapeTransform)
+    transformer.mapShapes(
+      transformer.replaceShapes(withMappedTraits, newShapes.asJava),
+      shapeTransform
+    )
   }
 
 }
