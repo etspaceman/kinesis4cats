4abbfc5673715560f9f51ace0be052814867e077
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 19:43:02 +0000

    Update amazon-kinesis-client to 2.5.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -65,7 +65,7 @@ object LibraryDependencies {
     }
 
     val kpl = "com.amazonaws" % "amazon-kinesis-producer" % "0.15.7"
-    val kcl = "software.amazon.kinesis" % "amazon-kinesis-client" % "2.4.8"
+    val kcl = "software.amazon.kinesis" % "amazon-kinesis-client" % "2.5.0"
   }
 
   object Cats {
