c1cbaa9a013d883c7d23fb482ee1ed99e847419f
Scala Steward <scala_steward@virtuslab.com>
2023-05-24 19:28:58 +0000

    Update http4s-circe, http4s-ember-client, ... to 0.23.19

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -88,7 +88,7 @@ object LibraryDependencies {
   }
 
   object Http4s {
-    val http4sVersion = "0.23.18"
+    val http4sVersion = "0.23.19"
     val emberServer =
       Def.setting("org.http4s" %%% "http4s-ember-server" % http4sVersion)
     val emberClient =
