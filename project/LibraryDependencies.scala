c433f37a89b7674b22f5043ce03ad1f3bdf3706c
Scala Steward <scala_steward@virtuslab.com>
2023-05-27 17:30:20 +0000

    Update cats-effect to 3.5.0

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -70,7 +70,7 @@ object LibraryDependencies {
 
   object Cats {
     val catsVersion = "2.9.0"
-    val catsEffectVersion = "3.4.11"
+    val catsEffectVersion = "3.5.0"
     val core = Def.setting("org.typelevel" %%% "cats-core" % catsVersion)
     val effect =
       Def.setting("org.typelevel" %%% "cats-effect" % catsEffectVersion)
