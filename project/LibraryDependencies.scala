15c436fc755cf9626eca89fce49ea40970f37643
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-09 15:28:34 +0200

    Update circe-core, circe-parser, ... to 0.14.6 (#209)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -77,7 +77,7 @@ object LibraryDependencies {
   }
 
   object Circe {
-    val circeVersion = "0.14.5"
+    val circeVersion = "0.14.6"
     val core = Def.setting("io.circe" %%% "circe-core" % circeVersion)
     val parser = Def.setting("io.circe" %%% "circe-parser" % circeVersion)
     val scodec = Def.setting("io.circe" %%% "circe-scodec" % circeVersion)
