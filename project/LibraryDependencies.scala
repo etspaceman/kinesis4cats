75fede565b548e8902173a861f061559e58b5e61
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-09-30 17:18:02 +0200

    Update feral-lambda to 0.2.4 (#224)

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -89,7 +89,7 @@ object LibraryDependencies {
   }
 
   object Feral {
-    val feralVersion = "0.2.3"
+    val feralVersion = "0.2.4"
     val lambda = Def.setting("org.typelevel" %%% "feral-lambda" % feralVersion)
   }
 
