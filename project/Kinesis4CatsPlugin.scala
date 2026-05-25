de567bddf94d8ec3893e24a130bc02fa10d8b0d8
Eric Meisel <eric.steven.meisel@gmail.com>
2026-05-24 22:13:54 -0500

    Bump tlBaseVersion from 0.2 to 0.3

--- project/Kinesis4CatsPlugin.scala
+++ project/Kinesis4CatsPlugin.scala
@@ -63,7 +63,7 @@ object Kinesis4CatsPlugin extends AutoPlugin {
   }
 
   override def buildSettings = Seq(
-    tlBaseVersion := "0.2",
+    tlBaseVersion := "0.3",
     organization := "io.github.etspaceman",
     organizationName := "etspaceman",
     startYear := Some(2023),
